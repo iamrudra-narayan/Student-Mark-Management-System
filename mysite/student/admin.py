9ed081c944496ee37bc1dfaa3dab9078ba22c0a4
Rudranarayan Muduli <84215414+iamrudra-narayan@users.noreply.github.com>
2022-12-10 10:27:29 +0530

    Delete mysite directory

--- mysite/student/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from student.models import student_detail, student_mark
-
-# Register your models here.
-admin.site.register(student_detail)
-admin.site.register(student_mark)
